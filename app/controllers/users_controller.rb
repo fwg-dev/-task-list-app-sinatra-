a44ea271386e5e8d18d1937fcd29b5e95e6d5796
Faith Gicheha <fwgicheh@gmail.com>
2020-08-05 15:10:25 -0700

    add logout method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     #they have the credentials - email/password combo
     if  @user.authenticate(params[:password])
     #log the user in - create the user sessions
-    session[:user_id] = @user.id
+    session[:user_id] = @user.id #actually login user in 
     #redirect to user's landing page (show? index? dashboard?)
     #undefined method "authenticate" for nil:NilClass- means that @user has a value of nil. so you have to enter valid data from seeds 
      puts session
@@ -29,16 +29,47 @@ class UsersController < ApplicationController
     end 
 
   end 
-   #what routes do I need for signup?
-
+   #what routes do I need for signup? 
+   #this route's job is to render a signup form 
   get '/signup' do 
+    #erb(render) a view 
 
+    erb :signup
+  end 
+
+  post '/users' do 
+    #here is where we will create a new user and persist the new 
+    #user to the DB
+    #params will look like: {"name"=>"fay", "email"=>"shiru@gmail.com", "password"=>"pw"}
+
+    #I only want to persist a user that has a name, email and password 
+    if params[:name] !="" && params[:email] != "" && params[:password] != ""
+      #valid
+      @user = User.create(params)
+      session[:user_id] = @user.id #actually login user in 
+
+      #where do I want to send my user - log user after signup  
+      #let's go to the user show page
+      redirect "/users/#{@user.id}"
+
+      # erb :'/users/show'
+    else 
+      #not valid 
+      #will include message to user- telling what is wrong 
+      redirect '/signup'
+    end 
   end 
 
   # user show route 
-
   get '/users/:id' do 
-    "this will be the user show route"
+    #what do I need to do first?
+    @user =User.find_by(id: params[:id])
+     
+    erb :'/users/show'
   end 
 
+  get '/logout' do 
+    session.clear 
+    redirect'/'
+  end 
 end 
