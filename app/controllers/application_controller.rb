25228201a57153c08a7b7304e5252defdbd20ad5
Faith Gicheha <fwgicheh@gmail.com>
2020-08-05 01:24:19 -0700

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions # layer of security. If someone trued to login without credentials they will not be able to 
+    set :session_secret, "our_task_list_app"
   end
 
   get "/" do
