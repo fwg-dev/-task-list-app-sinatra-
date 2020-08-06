dd987752c3d41fd3a0d5927eb2f43675b9a3a972
Faith Gicheha <fwgicheh@gmail.com>
2020-08-06 01:38:01 -0700

    add lists controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,18 @@
+class ListsController < ApplicationController 
+
+ #get list/new to render a form to create new entry
+
+ get '/lists/new' do 
+
+  erb :"/lists/new"
+ end 
+
+ #post list to create a new list entry 
+
+
+
+ #show page for a list entry 
+
+ #index route for all list entries 
+
+end 
