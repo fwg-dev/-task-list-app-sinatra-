6a9a7b136aec98e7770c81634516608625260ee9
Faith Gicheha <fwgicheh@gmail.com>
2020-08-06 15:19:56 -0700

    added lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,7 +9,23 @@ class ListsController < ApplicationController
 
  #post list to create a new list entry 
 
+ post '/lists' do 
 
+  #create a new journal entry and save it to db
+  #I only want to save the entry if it has content 
+  #I want to create a journal entry if user is logged in 
+
+  if logged_in?
+    redirect '/'
+  end 
+  if params[:title] != ""
+
+
+  else 
+
+  end 
+
+end 
 
  #show page for a list entry 
 
