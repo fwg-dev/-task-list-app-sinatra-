3d4923d76dbfbe6d5c3f2e8b3d54547333606130
Faith Gicheha <fwgicheh@gmail.com>
2020-08-10 16:32:16 -0700

    modify user db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,8 @@ kevin = User.create(name: "Kevin", email: "kevin@gmail.com", password: "pw")
 
 List.create(title: "shopping list", user_id: faith.id)
 
-List.create(title: "Movie and Tv list", user_id: faith.id)
-
 List.create(title: "travel packing list", user_id: kevin.id)
-
-List.create(title: "Restaurant list", user_id: kevin.id)
 
 Task.create(description:"Buy eggs", user_id: faith.id)
 
-Task.create(description:"Old Guard", user_id: faith.id)
-
-Task.create(description:"Pack jeans", user_id: kevin.id)
-
 Task.create(description:"Elephant Bar",user_id: kevin.id)
-
-
-#Task.create(description:"Buy eggs",description: "Buy Mangoes", description: "Buy milk" user_id: faith.id)
-
-#Task.create(description:"Old Guard", description:"Black is King", description: "Spencer" user_id: faith.id)
-
-#Task.create(description:"Pack jeans", description:"Pack shirts", description:"Pack toothbrush" user_id: kevin.id)
-
-#Task.create(description:"Elephant Bar", description:"In-and Out", description:"Chick-fil-A" user_id: kevin.id)
