24ceced4e669f120f5dc5ffb9abe442de2fa9d96
Faith Gicheha <fwgicheh@gmail.com>
2020-08-11 01:59:35 -0700

    delete tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_060539) do
+ActiveRecord::Schema.define(version: 2020_08_05_060357) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "description"
-    t.integer "due_date"
-    t.integer "user_id"
-    t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
