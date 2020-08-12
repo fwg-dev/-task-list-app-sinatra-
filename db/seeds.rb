622c60661b6981c63e517d91b31e13d9543764f2
Faith Gicheha <fwgicheh@gmail.com>
2020-08-12 15:49:22 -0700

    modify user db

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@ faith = User.create(name: "Faith", email: "faith@gmail.com", password: "pw")
 
 kevin = User.create(name: "Kevin", email: "kevin@gmail.com", password: "pw")
 
-List.create(title: "shopping list", description:"Buy eggs" user_id: faith.id)
+List.create(title: "shopping list", description:"Buy eggs", due_date: "8/12/20", user_id: faith.id)
 
-List.create(title: "travel packing list", description:"pack jeans" user_id: kevin.id)
+List.create(title: "travel packing list", description:"pack jeans" due_date: "8/18/20",user_id: kevin.id)
 
 # Task.create(description:"Buy eggs", user_id: faith.id)
 
