a80b4b54f60ad1cb40a6267e75c4b57dc612ae31
Faith Gicheha <fwgicheh@gmail.com>
2020-08-18 01:07:35 -0700

    remove comments

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,27 +1,21 @@
 class ListsController < ApplicationController 
 
-  #get list/new to render a form to create new entry
+  get '/lists' do
+    @lists = List.all 
+     erb :'lists/index'
+   end
+
+  #CREATE
   get '/lists/new' do 
       erb :"/lists/new"
   end 
-  
-  #post list to create a new list entry 
-
+   
   post '/lists' do 
-
-    #create a new journal entry and save it to db
-    #I only want to save the entry if it has content 
-    #I want to create a journal entry if user is logged in 
-
-    # if !logged_in?
-    #   redirect '/'
-    # end 
     redirect_if_not_logged_in
 
     if params[:title] != ""
-      #create a new entry 
       @list = List.create(title: params[:title], user_id: current_user.id, description: params[:description], due_date: params[:due_date])
-      flash[:message] = "You've created a task list successfully." if @list.id #only display if it was assigned an id 
+      flash[:message] = "You've created a task list successfully." if @list.id 
       redirect "/lists/#{@list.id}"
     else 
       flash[:error]= "Please provide valid input."
@@ -29,14 +23,12 @@ class ListsController < ApplicationController
     end 
   end 
   
-  #show page for a list entry 
+  #READ
   get '/lists/:id' do 
     set_list 
     erb :'/lists/show'
   end 
     
-  #This route should send us to list edit.erb, which would
-  #render to an edit form
   get '/lists/:id/edit' do
     set_list
     redirect_if_not_logged_in
@@ -47,42 +39,33 @@ class ListsController < ApplicationController
     end 
   end
 
-  #This action's job is to 
+  #UPDATE
   patch '/lists/:id' do
-   #1.find list entry 
     redirect_if_not_logged_in
     set_list 
-      #2. modify/update the list entry 
-    if @list.user == current_user && params[:title] &&  params[:description] &&  params[:due_date] != ""
+    if @list.user == current_user && params[:title] && params[:description] && params[:due_date] != ""
       @list.update(title: params[:title], description: params[:description], due_date: params[:due_date])
-      #3. redirect to show page 
       flash[:message] = "You've updated the task successfully"
        redirect "/lists/#{@list.id}" 
 
     else 
       flash[:error] ="Field cannot be empty. Please provide valid input"
       redirect "/lists/#{@list.id}/edit"
-    end  
+   end  
   end
 
-    #index route for all list entries 
-  get '/lists' do
-   @lists = List.all 
-    erb :'lists/index'
-  end
-
-  #delete entry 
+  #DELETE 
   delete '/lists/:id' do 
     flash[:message] = "You've deleted the task successfully"
     set_list
-    if authorized_to_edit?(@list)  #delete the entry #go to show page 
+    if authorized_to_edit?(@list) 
       @list.destroy
       redirect '/lists'
-    else #if you are not allowed to manage the delete--send the user back to
+    else 
       redirect '/lists'
     end 
-    #go somewhere else - not deleted 
   end 
+
 
   private 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,14 @@
 class UsersController < ApplicationController
 
-  #render form
-  #what routes do I need for login?
-  #purpose is to render login page(form)
   get '/login' do
     erb :login
   end
-  #the purpose of this route is to receive  the login form 
-  #find the user and log the user in (create a session) 
 
   post '/login' do 
   
     @user =User.find_by(email: params[:email])
-    #find the user 
-    #Authenticate the user- verify user is who they say they are. 
-    #they have the credentials - email/password combo
     if @user && @user.authenticate(params[:password])
-      #log the user in - create the user sessions
-      session[:user_id] = @user.id #actually login user in 
-      #redirect to user's landing page (show? index? dashboard?)
-      #undefined method "authenticate" for nil:NilClass- means that @user has a value of nil. so you have to enter valid data from seeds 
-    #  flash[:message] ="Welcome back #{user.name}!"
+      session[:user_id] = @user.id 
       puts session
       flash[:message] ="Welcome, #{@user.name}!"
      redirect "users/#{@user.id}"
@@ -28,49 +16,32 @@ class UsersController < ApplicationController
     else 
      
       flash[:error] ="Your credentials were invalid. Please signup or try again."
-      #tell user they entered invalid credentials 
-      #redirect them to the landing page (show? index? dashboard?)
-      #
       redirect '/login'
     end 
 
   end 
-   #what routes do I need for signup? 
-   #this route's job is to render a signup form 
+   
   get '/signup' do 
-    #erb(render) a view 
     erb :signup
   end 
 
   post '/users' do 
-    #here is where we will create a new user and persist the new 
-    #user to the DB
-    #params will look like: {"name"=>"fay", "email"=>"shiru@gmail.com", "password"=>"pw"}
-
-    #I only want to persist a user that has a name, email and password 
      @user = User.new(params)
     if @user.save      
      #if params[:name] !="" && params[:email] != "" && params[:password] != ""
       #valid
       # @user = User.create(params)      
-      session[:user_id] = @user.id #actually login user in 
-      flash[:message] = "You have successdully created an account, #{@user.name}! Welcome!"
-      #where do I want to send my user - log user after signup  
-      #let's go to the user show page
+      session[:user_id] = @user.id 
+      flash[:message] = "Welcome! Thanks for signing up, #{@user.name}!"
       redirect "/users/#{@user.id}"
 
-      # erb :'/users/show'
     else 
-      #not valid 
-      #will include message to user- telling what is wrong 
       flash[:error] = "Account creation failure: #{@user.errors.full_messages.to_sentence}" 
       redirect '/signup'
     end 
   end 
 
-  # user show route 
   get '/users/:id' do 
-    #what do I need to do first?
     @user =User.find_by(id: params[:id])
     erb :'/users/show'
   end 
